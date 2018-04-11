c0ee84be2e493180a3fd6346d9fc901c40db94f6
Jacob Heuser <jacob.heuser@student.uibk.ac.at>
2018-04-11 17:25:52 +0200

    javascript 101 bis datentypen

---
+++ main.js
@@ -0,0 +1,31 @@
+//alert("hello world");
+let nachricht = "hallo welt";
+//alert(nachricht);
+nachricht = "neuer Text";
+//alert(nachricht);
+console.log(nachricht)
+//farbe definieren
+const linkColor ="#ff0000";
+//varibalename darf nicht mit einer zahl beginnen, aber enthalten, typisch auf englisch, keine bindestriche. let als einleitung für eine variable muss nur beim ersten mal angeben werden.
+let eine_variable = "testx";
+console.log("Variable: eine_variable: ", eine_variable);
+//zeichen-datentyp
+let highScore = 75994;
+console.log("Highscore ist: ", highScore / 20);
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = "test2"
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+und 
+ein 
+ganz 
+langer Textund aus`;
+//für zeilenumbrüche braucht man das einfach Anführungszeichen 
+console.log(meineNachricht);
+let fullName = "Jeffrey `the Dude`Lebowsky";
+console.log(fullName); 
+let isOver18 = true;
+console.log("über18?", isOver18);
+let age = 17;
+console.log("age über 18", age > 18);
